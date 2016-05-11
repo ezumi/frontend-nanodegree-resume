6433e2d49c5e256c51a2a00cca89cac11e16c6e1
Esmond Kan <ek.public@protonmail.com>
2016-05-10 23:17:19 -0700

    refactor: Refactor JS code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,9 +9,9 @@ var bio = {
         "location": "Vancouver, British Columbia"
     },
     "welcomeMessage": "'It's all monkey business here!'",
-    "skills": ["HTML5", "CSS3", "Javascript", "jQuery","Photoshop", "Bootstrap"],
+    "skills": ["HTML5", "CSS3", "Javascript", "jQuery", "Photoshop", "Bootstrap"],
     "bioPic": "images/monkey-hero.svg",
-     display: function() {
+    display: function() {
 
         /* Name and Role */
         $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
@@ -50,43 +50,36 @@ var bio = {
             $("#skills-list").append(HTMLskills.replace("%data%", skillData));
         });
     }
-}
+};
 
 var education = {
-    "schools" : [
-        {
-            "name": "Vancouver Career College",
-            "location": "5021 Kingsway, Burnaby, BC, V5H 4A5",
-            "degree": "Diploma",
-            "majors": "Computer Systems Programmer",
-            "dates": "2000 - 2001",
-            "url": "http://www.vccollege.ca/"
-        },
-        {
-            "name": "British Columbia Institute of Technology",
-            "location": "3700 Willingdon Avenue, Burnaby, BC, V5G 3H2",
-            "degree": "Diploma",
-            "majors": "Computer Systems Technology",
-            "dates": "2003 - 2004",
-            "url": "http://www.bcit.ca/"
-        }
-    ],
-    "onlineCourses" : [
-        {
-            "title": "Front-End Web Developer Nanodegree",
-            "school": "Udacity",
-            "date": 2016,
-            "url": "http://www.udacity.com/"
-        }
-    ],
-    display : function() {
+    "schools": [{
+        "name": "Vancouver Career College",
+        "location": "5021 Kingsway, Burnaby, BC, V5H 4A5",
+        "degree": "Diploma",
+        "majors": "Computer Systems Programmer",
+        "dates": "2000 - 2001",
+        "url": "http://www.vccollege.ca/"
+    }, {
+        "name": "British Columbia Institute of Technology",
+        "location": "3700 Willingdon Avenue, Burnaby, BC, V5G 3H2",
+        "degree": "Diploma",
+        "majors": "Computer Systems Technology",
+        "dates": "2003 - 2004",
+        "url": "http://www.bcit.ca/"
+    }],
+    "onlineCourses": [{
+        "title": "Front-End Web Developer Nanodegree",
+        "school": "Udacity",
+        "date": 2016,
+        "url": "http://www.udacity.com/"
+    }],
+    display: function() {
         education.schools.forEach(function(schoolData, index) {
             $("#education").append(HTMLschoolStart);
 
             /* Replace URL and school name then concatenate degree to string */
-            var formattedSchool = HTMLschoolName.replace("#", education.schools[index].url)
-                                                .replace("%data%", education.schools[index].name)
-                                + HTMLschoolDegree.replace("%data%", education.schools[index].degree);
+            var formattedSchool = HTMLschoolName.replace("#", education.schools[index].url).replace("%data%", education.schools[index].name) + HTMLschoolDegree.replace("%data%", education.schools[index].degree);
 
             $(".education-entry:last").append(formattedSchool);
             $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[index].dates));
@@ -94,16 +87,14 @@ var education = {
             $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[index].majors));
         });
 
-        if(education.onlineCourses.length > 0) {
+        if (education.onlineCourses.length > 0) {
             $("#education").append(HTMLonlineClasses);
 
             education.onlineCourses.forEach(function(onlineData, index) {
                 $("#education").append(HTMLschoolStart);
 
                 /* Replace URL and program name then concatenate school name to string */
-                var formattedOnlineClasses = HTMLonlineTitle.replace("#", education.onlineCourses[index].url)
-                                                            .replace("%data%", education.onlineCourses[index].title)
-                                           + HTMLonlineSchool.replace("%data%", education.onlineCourses[index].school);
+                var formattedOnlineClasses = HTMLonlineTitle.replace("#", education.onlineCourses[index].url).replace("%data%", education.onlineCourses[index].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[index].school);
 
                 $(".education-entry:last").append(formattedOnlineClasses);
                 $(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[index].date));
@@ -111,35 +102,30 @@ var education = {
             });
         }
     }
-}
+};
 
 var work = {
-    "jobs": [
-        {
-            "employer": "Metro West Insurance Agency Ltd",
-            "title": "IT Technical Support",
-            "location": "206 E Columbia Street, New Westminster, BC, V3L 3W5",
-            "dates": "2006 - 2015",
-            "description": "Deploy, configure and maintain computer systems at the company.<br>Provide assistance to agents in resolving their technical issues.",
-            "url": "http://metrowestinsurance.net/"
-        },
-        {
-            "employer": "PushMedia",
-            "title": "Marketing Project Manager",
-            "location": "#285 - 4111 Hastings Street, Burnaby, BC, V5C 6T7",
-            "dates": "2013 - Present",
-            "description": "Develop and integrate advertising campaigns targeting various online mediums including web, social media, mobile and display network traffic.<br>Design web visuals and graphic assets for marketing projects.<br>Create fully responsive landing pages utilizing HTML, CSS, Javascript and jQuery.<br>Optimize deployed campaigns with revised marketing strategies to increase return of investment.",
-            "url": "http://pushmedia.ca/"
-        }
-    ],
-    display : function() {
+    "jobs": [{
+        "employer": "Metro West Insurance Agency Ltd",
+        "title": "IT Technical Support",
+        "location": "206 E Columbia Street, New Westminster, BC, V3L 3W5",
+        "dates": "2006 - 2015",
+        "description": "Deploy, configure and maintain computer systems at the company.<br>Provide assistance to agents in resolving their technical issues.",
+        "url": "http://metrowestinsurance.net/"
+    }, {
+        "employer": "PushMedia",
+        "title": "Marketing Project Manager",
+        "location": "#285 - 4111 Hastings Street, Burnaby, BC, V5C 6T7",
+        "dates": "2013 - Present",
+        "description": "Develop and integrate advertising campaigns targeting various online mediums including web, social media, mobile and display network traffic.<br>Design web visuals and graphic assets for marketing projects.<br>Create fully responsive landing pages utilizing HTML, CSS, Javascript and jQuery.<br>Optimize deployed campaigns with revised marketing strategies to increase return of investment.",
+        "url": "http://pushmedia.ca/"
+    }],
+    display: function() {
         work.jobs.forEach(function(workData, index) {
             $("#workExperience").append(HTMLworkStart);
 
             /* Replace URL and employter name then concatenate job title to string */
-            var formattedEmployer = HTMLworkEmployer.replace("%url%", work.jobs[index].url)
-                                                    .replace("%data%", work.jobs[index].employer)
-                                  + HTMLworkTitle.replace("%data%", work.jobs[index].title);
+            var formattedEmployer = HTMLworkEmployer.replace("%url%", work.jobs[index].url).replace("%data%", work.jobs[index].employer) + HTMLworkTitle.replace("%data%", work.jobs[index].title);
 
             $(".work-entry:last").append(formattedEmployer);
             $(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[index].dates));
@@ -147,38 +133,35 @@ var work = {
             $(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[index].description));
         });
     }
-}
+};
 
 var projects = {
-    "project" : [
-        {
-            "title": "Portfolio Webpage",
-            "date": "4/2016",
-            "description": "Personal portofolio to showcase the latest projects I have completed.",
-            "images": ["images/portfolio1.png"]
-        },
-        {
-            "title": "Upcoming Projects",
-            "date": "2016",
-            "description": "Come back for more projects I plan to complete in the near future.",
-            "images": ["images/197x148.gif", "images/197x148.gif"]
-        }
-    ],
-    display : function() {
+    "project": [{
+        "title": "Portfolio Webpage",
+        "date": "4/2016",
+        "description": "Personal portofolio to showcase the latest projects I have completed.",
+        "images": ["images/portfolio1.png"]
+    }, {
+        "title": "Upcoming Projects",
+        "date": "2016",
+        "description": "Come back for more projects I plan to complete in the near future.",
+        "images": ["images/197x148.gif", "images/197x148.gif"]
+    }],
+    display: function() {
         projects.project.forEach(function(projectData, index) {
             $("#projects").append(HTMLprojectStart);
             $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.project[index].title));
             $(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.project[index].date));
             $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.project[index].description));
 
-            if(projects.project[index].images.length > 0) {
-                projects.project[index].images.forEach(function(imageData, imageIndex){
+            if (projects.project[index].images.length > 0) {
+                projects.project[index].images.forEach(function(imageData, imageIndex) {
                     $(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.project[index].images[imageIndex]));
                 });
             }
         });
     }
-}
+};
 
 /*
  * This function checks to see if a scroll occurs and adds or removes the appropriate classes
@@ -194,8 +177,7 @@ $(window).scroll(function() {
             $('.navbar').addClass('navbar-scroll-grow');
             $('.menu-item').addClass('menu-item-scroll');
         }
-    }
-    else {
+    } else {
         // If max width is greater than 650
         if (!mq) {
             $('.navbar').removeClass('navbar-scroll');
@@ -236,10 +218,13 @@ $(window).resize(function() {
         if (($('.navbar').hasClass('navbar-scroll')) && ($('.navbar').offset().top < 60)) {
             $('.navbar').removeClass('navbar-scroll');
         }
-        $('#menu').css({'display': 'flex'});
-    }
-    else {
-        $('#menu').css({'display': 'none'});
+        $('#menu').css({
+            'display': 'flex'
+        });
+    } else {
+        $('#menu').css({
+            'display': 'none'
+        });
     }
 });
 
@@ -249,29 +234,29 @@ $(window).resize(function() {
  */
 
 function check_if_in_view() {
-  var $animation_elements = $('.animation-element');
-  var $window = $(window);
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+    var $animation_elements = $('.animation-element');
+    var $window = $(window);
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+    $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
 
-    // Get animation class to add from data attribute in DOM
-    var animation_class = $element.data("animation-class");
+        // Get animation class to add from data attribute in DOM
+        var animation_class = $element.data("animation-class");
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass(animation_class);
-    } else {
-      $element.removeClass(animation_class);
-    }
-  });
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass(animation_class);
+        } else {
+            $element.removeClass(animation_class);
+        }
+    });
 }
 
 /*
